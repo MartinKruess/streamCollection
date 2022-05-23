75623ad8e370a3c45e06369c96b809f95002ca7d
Martin <raikunsgamezone@gmx.de>
2022-05-23 11:06:50 +0200

    routerFixed for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { UserProvider } from "./comps/context/userContext"
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+  //<Router>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  //</Router>
 )
