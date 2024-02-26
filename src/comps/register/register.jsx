eb8657bd72bed21bd45fe50877ec719752aacaee
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2024-02-26 14:17:21 +0100

    version WE

--- src/comps/register/register.jsx
+++ src/comps/register/register.jsx
@@ -1,15 +1,16 @@
 import "../../App.scss";
-import { Link } from "react-router-dom";
-import { useState } from 'react';
-import { inputTemp } from "./registerInputs"
-import { FormInputs } from "./formInputs"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { inputTemp } from "./registerInputs";
+import { FormInputs } from "./formInputs";
 import { generatePassword } from "./passwordGenerator";
 import { fetchURL } from "../../App";
 
 export const Register = () => {
   const onChange = (e) => {
-    setRegisterData({ ...registerData, [e.target.name]: e.target.value })
-  }
+    setRegisterData({ ...registerData, [e.target.name]: e.target.value });
+  };
+  const navigate = useNavigate();
 
   const [registerData, setRegisterData] = useState({
     email: "",
@@ -18,38 +19,47 @@ export const Register = () => {
     username: "",
     password: "",
     confirmPassword: "",
-  })
+  });
 
   const getPassword = async () => {
-    const newPassword = await generatePassword()
-    setRegisterData({ ...registerData, "confirmPassword": newPassword, "password": newPassword })
+    const newPassword = await generatePassword();
+    setRegisterData({
+      ...registerData,
+      confirmPassword: newPassword,
+      password: newPassword,
+    });
   };
 
-  const password = registerData.password === registerData.confirmPassword && registerData.password.length > 9
-  const isFilled = registerData.email.length > 5 && registerData.firstname.length > 2 && registerData.lastname.length > 5 && registerData.username.length > 5
+  const password =
+    registerData.password === registerData.confirmPassword &&
+    registerData.password.length > 9;
+  const isFilled =
+    registerData.email.length > 5 &&
+    registerData.firstname.length > 2 &&
+    registerData.lastname.length > 5 &&
+    registerData.username.length > 5;
 
   if (password === true && isFilled === true) {
-    console.log("YES")
+    console.log("YES");
   } else {
-    console.log("NO")
+    console.log("NO");
   }
 
   const getUserId = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // const isValidatated = validateData(registerData)
     // if (isValidatated === false) return
 
-    const result = await fetch(`${fetchURL}/user/register`,
-      {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(registerData)
-      }
-    )
-  }
+    const result = await fetch(`${fetchURL}/user/register`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registerData),
+    });
+    navigate("/login");
+  };
 
   return (
     <section>
@@ -58,15 +68,24 @@ export const Register = () => {
           <h2 className="dark">Sign Up</h2>
           <form className="form" onSubmit={(e) => getUserId(e)}>
             {inputTemp.map((input) => (
-              <FormInputs key={input.id} {...input} value={registerData[inputTemp.name]} onChange={onChange} />
+              <FormInputs
+                key={input.id}
+                {...input}
+                value={registerData[inputTemp.name]}
+                onChange={onChange}
+              />
             ))}
             {/* registerData.password ?  */}
-            <input type="button" value="Generate Password" onClick={() => getPassword()} />
+            <input
+              type="button"
+              value="Generate Password"
+              onClick={() => getPassword()}
+            />
             <input type="submit" value="Sign Up" />
             <Link to="/login"></Link>
           </form>
         </div>
       </article>
     </section>
-  )
-}
+  );
+};
