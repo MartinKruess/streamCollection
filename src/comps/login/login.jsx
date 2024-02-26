f11b31eb0de75dbfd1a869f6015cadcaace83d21
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2024-02-26 10:25:37 +0100

    login fetch JSON problem

--- src/comps/login/login.jsx
+++ src/comps/login/login.jsx
@@ -24,7 +24,7 @@ export const Login = () => {
       credentials: "include",
 
       headers: {
-        Accept: "application/json",
+        // Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(loginData),
